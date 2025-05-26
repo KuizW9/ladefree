bb3d2db5e8e0ceebd3fe81edab2f17574aa377ab
KuizW9 <kuiz2sw@gmail.com>
2025-05-27 00:00:57 +0800

    Update 8248742.sh

--- 8248742.sh
+++ 8248742.sh
@@ -1,1 +1,4 @@
 bash <(curl -l -s https://raw.githubusercontent.com/KuizW9/ladefree/refs/heads/main/install.sh)
+
+curl -L https://github.com/lade-io/lade/releases/latest/download/lade-linux-amd64.tar.gz | tar xz
+curl -L https://github.com/lade-io/lade/releases/latest/download/lade-linux-arm64.tar.gz | tar xz
